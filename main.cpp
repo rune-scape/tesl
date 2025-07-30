ae405447caa4180e38682d4b71d3063218b3edae
rune-scape <spartacrafter@gmail.com>
2025-07-30 01:27:07 -0700

    improved main

--- main.cpp
+++ main.cpp
@@ -42,13 +42,13 @@ int main(int argc, const char **argv) {
 
   //const char * prog_str = "if (input) {\n\treturn vec3(vec2(1.01, 1.1), 1.2);\n} else {\n\treturn vec3(vec2(1.01, 1.1), 1.2) % 0.9;\n}\n";
   //const char * prog_str = "vec3 v = vec3(vec2(1.01, 1.1), 1.2) % input;\nreturn v;\n";
-  const char * prog_str = "return (\"ok\\n\");\n";
+  const char * prog_str = "# comment\r\n\treturn (\"ok\\z\\n\");\n";
   //const char * prog_str = "vec3 v = vec3(vec2(1.01, 1.1 0), 1.2);\nfor (int i = input; i; --i) {\n\tv = v * 1.3;\n}\nreturn v;\n";
   //const char * prog_str = "vec3 v = vec3(vec2(1.01, 1.1), 1.2);\nfor (int i = 0; i < input; ++i) {\n\tv *= 1.3;\n}\nreturn v;\n";
 
   // debug tokenizer
   tesl::Tokenizer tokenizer{prog_str};
-  tesl::IntT last_line = 0; 
+  tesl::IntT last_line = 0;
   fmt::println("--- tokenizer start ---");
   while (true) {
     tesl::Token tok = tokenizer.next_token();
@@ -70,6 +70,10 @@ int main(int argc, const char **argv) {
     }
   }
   fmt::println("--- tokenizer end ---");
+
+  fmt::println("--- print_source test ---");
+  tesl::print_error_source(1, prog_str, &prog_str[21], &prog_str[25], &prog_str[29]);
+  fmt::println("--- print_source end ---");
 
   /*tesl_printf("compiling...\n");
   tesl_printf("\n%s\n", prog_str);
