cbe510b992a37b51a0dfa258a4ad8d2eddc72dd1
rune-scape <spartacrafter@gmail.com>
2025-07-30 06:02:37 -0700

    main++

--- main.cpp
+++ main.cpp
@@ -42,7 +42,7 @@ int main(int argc, const char **argv) {
 
   //const char * prog_str = "if (input) {\n\treturn vec3(vec2(1.01, 1.1), 1.2);\n} else {\n\treturn vec3(vec2(1.01, 1.1), 1.2) % 0.9;\n}\n";
   //const char * prog_str = "vec3 v = vec3(vec2(1.01, 1.1), 1.2) % input;\nreturn v;\n";
-  const char * prog_str = "# comment\r\n\treturn (\"ok\\z\\n\");\n";
+  const char * prog_str = "# comment\r\n  return (\"ok\\z\\n\");\n";
   //const char * prog_str = "vec3 v = vec3(vec2(1.01, 1.1 0), 1.2);\nfor (int i = input; i; --i) {\n\tv = v * 1.3;\n}\nreturn v;\n";
   //const char * prog_str = "vec3 v = vec3(vec2(1.01, 1.1), 1.2);\nfor (int i = 0; i < input; ++i) {\n\tv *= 1.3;\n}\nreturn v;\n";
 
