2a22e811831d98dce08de2f28c47529d382667fe
rune-scape <spartacrafter@gmail.com>
2025-07-29 21:21:34 -0700

    print_error_source++

--- tesl_common.cpp
+++ tesl_common.cpp
@@ -1,83 +1,7 @@
 #include "tesl_common.hpp"
-#include "unicode.hpp"
+#include "tesl_fmt.hpp"
 
 namespace tesl {
-  /*void print(const char16_t * str) {
-    using namespace unicode;
-    char buffer[256];
-
-    while (*str) {
-      char * output_ptr = buffer;
-      char * output_end = buffer + sizeof(buffer) - 1;
-      codec_error e = codec_error::ok;
-      for (; *str && !(e & codec_error::insufficient_space); ++str) {
-        char32_t cp = U'\0';
-        e |= utf16::decode_next(cp, str);
-        e |= utf8::encode(cp, output_ptr, output_end);
-      }
-      *output_ptr = '\0';
-    }
-
-    print(buffer);
-  }
-
-  void print(const char32_t * str) {
-    using namespace unicode;
-    char buffer[256];
-
-    while (*str) {
-      char * output_ptr = buffer;
-      char * output_end = buffer + sizeof(buffer) - 1;
-      codec_error e = codec_error::ok;
-      for (; *str && !(e & codec_error::insufficient_space); ++str) {
-        e |= utf8::encode(*str, output_ptr, output_end);
-      }
-      *output_ptr = '\0';
-    }
-
-    print(buffer);
-  }
-
-  void print(const Char16StrView & str) {
-    using namespace unicode;
-    char buffer[256];
-
-    const char16_t * input_ptr = str.data();
-    const char16_t * input_end = str.end();
-    while (input_ptr != input_end) {
-      char * output_ptr = buffer;
-      char * output_end = buffer + sizeof(buffer) - 1;
-      codec_error e = codec_error::ok;
-      for (; input_ptr != input_end && !(e & codec_error::insufficient_space); ++input_ptr) {
-        char32_t cp = U'\0';
-        e |= utf16::decode_next(cp, input_ptr, input_end);
-        e |= utf8::encode(cp, output_ptr, output_end);
-      }
-      *output_ptr = '\0';
-    }
-
-    print(buffer);
-  }
-
-  void print(const Char32StrView & str) {
-    using namespace unicode;
-    char buffer[256];
-
-    const char32_t * input_ptr = str.data();
-    const char32_t * input_end = str.end();
-    while (input_ptr != input_end) {
-      char * output_ptr = buffer;
-      char * output_end = buffer + sizeof(buffer) - 1;
-      codec_error e = codec_error::ok;
-      for (; input_ptr != input_end && !(e & codec_error::insufficient_space); ++input_ptr) {
-        e |= utf8::encode(*input_ptr, output_ptr, output_end);
-      }
-      *output_ptr = '\0';
-    }
-
-    print(buffer);
-  }*/
-
   void print_error_sourcev(FILE * file, int line_num, const char * line_start, const char * start, const char * point, const char * const end) {
     assert(start <= point && point <= end);
     const char * line_end = line_start;
@@ -94,9 +18,7 @@ namespace tesl {
           int highlight_begin_length = point - highlight_start;
           const int highlight_end_length = max<int>(1, highlight_end - point);
           fmt::print(file, "{:~<{}}", "", highlight_begin_length);
-          if (highlight_end_length > 0) {
-            fmt::println(file, "^{:~<{}}", "", highlight_end_length - 1);
-          }
+          fmt::println(file, "^{:~<{}}", "", highlight_end_length - 1);
         } else {
           const int highlight_length = highlight_end - highlight_start;
           fmt::println(file, "{:~<{}}", "", highlight_length);
